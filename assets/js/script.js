094a161df29d2a84caa4dd3644678208cabf87cc
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2024-11-29 19:11:43 +0100

    ultimo ultimo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ const Header= document.getElementById("headerColorata");
 const btnStart= document.getElementById("btnGetStarted");
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 350) {
+    if (window.scrollY > 180) {
         Header.style.backgroundColor = 'white';
     } else {
         Header.style.backgroundColor = '#ffc017';
     }
 });
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 350) {
+    if (window.scrollY > 180) {
         btnStart.style.backgroundColor = '#1a8917';
     } else {
         btnStart.style.backgroundColor = 'black';
